94fb195d936afcba2b79dc649a3efe1f85bdae51
Rohit Kumar <rohit.jit76@gmail.com>
2024-06-16 20:34:38 +0700

    c

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
                 <h2 className='text-2xl md:text-3xl font-bold mb-4 text-center'>Contact</h2>
                 <p><b>Email:</b> <a href="royrohit9402@gmail.com">royrohit9402@gmail.com</a></p>
                 <p><b>LinkedIn:</b> <a href="https://www.linkedin.com/in/rohitkumar76/">rohitkumar76</a></p>
-                <p><b>Mobile No:</b> <a href="tel">+91 7651893226</a></p>
+                <p><b>Mobile No:</b> <a href="tel:+917651893226">+91 7651893226</a></p>
             </div>
         </section>
     );
